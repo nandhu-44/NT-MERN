fde5134eaed2ce06bf39f0f0fcbc24c3aa282167
nandhu-44 <nandhu90040@gmail.com>
2024-02-27 20:20:07 +0530

    Add Supabase integration and language selection feature

--- Week3/day1/netflix/src/components/Header.jsx
+++ Week3/day1/netflix/src/components/Header.jsx
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const allLanguages = [
+    { name: "english", value: "English" },
+    { name: "hindi", value: "Hindi" },
+    { name: "malayalam", value: "Malayalam" },
+    { name: "tamil", value: "Tamil" },
+    { name: "arabic", value: "Arabic" },
+    { name: "french", value: "French" },
+    { name: "german", value: "German" },
+    { name: "italian", value: "Italian" },
+    { name: "japanese", value: "Japanese" },
+    { name: "korean", value: "Korean" },
+    { name: "mandarin", value: "Mandarin" },
+    { name: "portuguese", value: "Portuguese" },
+    { name: "russian", value: "Russian" },
+    { name: "spanish", value: "Spanish" },
+  ];
+
+  const storedLanguage = localStorage.getItem("language");
+  if (!storedLanguage) localStorage.setItem("language", allLanguages[0].name);
+
+  const [language, setLanguage] = useState(
+    storedLanguage || allLanguages[0].name,
+  );
+
+  const handleLanguageChange = (event) => {
+    let lang = event.target.value;
+    setLanguage(lang);
+    localStorage.setItem("language", lang);
+  };
+
   return (
     <header className="flex flex-row justify-between bg-neutral-900">
       <Link to="/" className="flex flex-row items-center">
-      <img
-        className="w-44 px-4 py-2"
-        src="assets/nflxlogo.webp"
-        alt="Netflix"
-      />
+        <img
+          className="w-44 px-4 py-2"
+          src="assets/nflxlogo.webp"
+          alt="Netflix"
+        />
       </Link>
       <div className="justify-auto flex flex-row items-center space-x-4 px-4 py-3 hover:cursor-pointer">
         <select
           name="languages"
           id="languages"
-          className="rounded-lg border-2 border-neutral-900 bg-red-500 px-5 py-2 font-bold  text-white hover:border-red-500 hover:bg-neutral-900"
+          className="rounded-lg border-2  border-neutral-900 bg-red-500 px-3 py-2 font-bold  text-white hover:border-red-500 hover:bg-neutral-900"
+          value={language}
+          onChange={handleLanguageChange}
         >
-          <option className="bg-neutral-900" value="en">
-            English
-          </option>
-          <option className="bg-neutral-900" value="hi">
-            Hindi
-          </option>
-          <option className="bg-neutral-900" value="te">
-            Telugu
-          </option>
-          <option className="bg-neutral-900" value="ar">
-            Arabic
-          </option>
+          {allLanguages.map((lang) => (
+            <option className="bg-neutral-900 text-green-400" value={lang.name}>
+              {lang.value}
+            </option>
+          ))}
         </select>
-        <Button text="Sign Up" to="/signup"/>
-        <Button text="Sign In" to="/signin"/>
+        <Button text="Sign Up" to="/signup" />
+        <Button text="Sign In" to="/signin" />
       </div>
     </header>
   );
---
+++ Week3/day1/netflix/src/database/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_API_KEY } = process.env;
+const supabaseClient = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_API_KEY);
+
+export default supabaseClient;
