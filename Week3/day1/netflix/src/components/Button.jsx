7649adfa3d5aa505375a189088539325ac0101a0
nandhu-44 <nandhu90040@gmail.com>
2024-03-02 13:06:51 +0530

    🍔Hamburger

--- Week3/day1/netflix/src/components/Button.jsx
+++ Week3/day1/netflix/src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button(props) {
   return (
     <Link
       to={props.to ?? "/"}
-      className="cursor-pointer rounded-lg border-2 border-red-500  px-2 py-1 md:px-5 md:py-2 text-xs font-bold text-white hover:bg-red-500 md:text-sm lg:text-base"
+      className="w-28 cursor-pointer rounded-lg border-2 border-red-500 px-6 py-1 text-center text-xs font-medium text-white hover:bg-red-500 md:w-auto md:px-5 md:py-2 md:text-sm md:font-bold lg:text-base"
     >
       {props?.text ?? "Button"}
     </Link>
--- Week3/day1/netflix/src/components/Header.jsx
+++ Week3/day1/netflix/src/components/Header.jsx
@@ -3,8 +3,6 @@ import Button from "./Button";
 import { Link } from "react-router-dom";
 
 function Header() {
-  const [showMenu, setShowMenu] = useState(false);
-
   const allLanguages = [
     { name: "english", value: "English" },
     { name: "hindi", value: "Hindi" },
@@ -35,74 +33,116 @@ function Header() {
     localStorage.setItem("language", lang);
   };
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+  // Hamburger handling
+  const hamburgerOpenButton = document.getElementById("hamburger-open");
+  const hamburgerCloseButton = document.getElementById("hamburger-close");
+  const hamburgerExpansion = document.getElementById("hamburger-expansion");
+
+  function hamburgerExpand() {
+    hamburgerExpansion.classList.toggle("hidden");
+    hamburgerExpansion.classList.toggle("flex");
+    hamburgerOpenButton.classList.toggle("hidden");
+    hamburgerCloseButton.classList.toggle("hidden");
+  }
+
+  function hamburgerClose() {
+    hamburgerExpansion.classList.toggle("hidden");
+    hamburgerExpansion.classList.toggle("flex");
+    hamburgerOpenButton.classList.toggle("hidden");
+    hamburgerCloseButton.classList.toggle("hidden");
+  }
 
   return (
-    <header className="flex flex-row justify-between bg-neutral-900">
-      <Link to="/" className="flex flex-row items-center">
-        <img
-          className="w-20 px-2 py-2 md:w-32 lg:w-44 lg:px-4"
-          src="assets/nflxlogo.webp"
-          alt="Netflix"
-        />
-      </Link>
-      <div className="justify-auto flex flex-row items-center space-x-4 px-4 py-3 hover:cursor-pointer">
-        <select
-          name="languages"
-          id="languages"
-          className="rounded-lg border-2  border-neutral-900 bg-red-500 px-3 py-2 text-xs font-bold text-white hover:border-red-500  hover:bg-neutral-900 md:text-base lg:text-xl"
-          value={language}
-          onChange={handleLanguageChange}
-        >
-          {allLanguages.map((lang) => (
-            <option className="bg-neutral-900 text-red-400" value={lang.name}>
-              {lang.value}
-            </option>
-          ))}
-        </select>
+    <>
+      <header className="flex flex-row justify-between bg-neutral-900">
+        <Link to="/" className="flex flex-row items-center">
+          <img
+            className="w-20 px-2 py-2 md:w-32 lg:w-44 lg:px-4"
+            src="assets/nflxlogo.webp"
+            alt="Netflix"
+          />
+        </Link>
         <div className="justify-auto hidden flex-row items-center space-x-4 px-4 py-3 hover:cursor-pointer sm:flex">
+          <select
+            name="languages"
+            id="languages"
+            className="rounded-lg border-2  border-neutral-900 bg-red-500 px-3 py-2 text-xs  font-normal text-white hover:border-red-500 hover:bg-neutral-900  md:text-base md:font-bold lg:text-base"
+            value={language}
+            onChange={handleLanguageChange}
+          >
+            {allLanguages.map((lang) => (
+              <option className="bg-neutral-900 text-red-400" value={lang.name}>
+                {lang.value}
+              </option>
+            ))}
+          </select>
           <Button text="Sign Up" to="/signup" />
           <Button text="Sign In" to="/signin" />
         </div>
-        <div className="block sm:hidden">
+        <button
+          className="block sm:hidden"
+          id="hamburger-open"
+          onClick={() => hamburgerExpand()}
+        >
           <svg
-            className="h-6 w-6"
-            viewBox="0 0 24 24"
-            fill="none"
+            class="h-[24px] w-[24px] text-gray-800 dark:text-white"
+            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
+            <path
+              stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {" "}
-              <path
-                opacity="0.5"
-                d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22Z"
-                fill="#ffffff"
-              ></path>{" "}
-              <path
-                d="M18.75 8C18.75 8.41421 18.4142 8.75 18 8.75H6C5.58579 8.75 5.25 8.41421 5.25 8C5.25 7.58579 5.58579 7.25 6 7.25H18C18.4142 7.25 18.75 7.58579 18.75 8Z"
-                fill="#ffffff"
-              ></path>{" "}
-              <path
-                d="M18.75 12C18.75 12.4142 18.4142 12.75 18 12.75H6C5.58579 12.75 5.25 12.4142 5.25 12C5.25 11.5858 5.58579 11.25 6 11.25H18C18.4142 11.25 18.75 11.5858 18.75 12Z"
-                fill="#ffffff"
-              ></path>{" "}
-              <path
-                d="M18.75 16C18.75 16.4142 18.4142 16.75 18 16.75H6C5.58579 16.75 5.25 16.4142 5.25 16C5.25 15.5858 5.58579 15.25 6 15.25H18C18.4142 15.25 18.75 15.5858 18.75 16Z"
-                fill="#ffffff"
-              ></path>{" "}
-            </g>
+              stroke-width="2"
+              d="m8 7 4 4 4-4m-8 6 4 4 4-4"
+            />
           </svg>
+        </button>
+        <button
+          className="hidden sm:hidden"
+          id="hamburger-close"
+          onClick={() => hamburgerClose()}
+        >
+          <svg
+            class="h-[24px] w-[24px] text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m16 17-4-4-4 4m8-6-4-4-4 4"
+            />
+          </svg>
+        </button>
+        {/* </div> */}
+      </header>
+      <div className="hidden flex-col bg-neutral-900" id="hamburger-expansion">
+        <div className="flex flex-col items-end justify-end space-y-4 px-3 py-2 hover:cursor-pointer sm:hidden">
+          <select
+            name="languages"
+            id="languages"
+            className="rounded-lg border-2  border-neutral-900 bg-red-500 px-3 py-2 text-xs text-center font-medium text-white hover:border-red-500  hover:bg-neutral-900 md:text-base lg:text-base"
+            value={language}
+            onChange={handleLanguageChange}
+          >
+            {allLanguages.map((lang) => (
+              <option className="bg-neutral-900 text-red-400" value={lang.name}>
+                {lang.value}
+              </option>
+            ))}
+          </select>
+          <Button text="Sign Up" to="/signup" />
+          <Button text="Sign In" to="/signin" />
         </div>
       </div>
-    </header>
+    </>
   );
 }
 
