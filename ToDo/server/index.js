f50724bb4c8914bbe3d2abc9031a1e5901a71d0a
nandhu-44 <nandhu90040@gmail.com>
2024-04-07 10:37:41 +0530

    Vercel config updated

--- ToDo/server/index.js
+++ ToDo/server/index.js
@@ -7,6 +7,13 @@ const routes = require('./routes/route');
 dotenv.config();
 
 app.use(bodyParser.json());
+app.use(cors(
+    {
+        origin: '*',
+        methods: 'GET, POST, PUT, DELETE',
+        allowedHeaders: 'Content-Type, Authorization'
+    }
+));
 
 console.clear();
 
