247ab9c89663d4b6c1b11c0fc5b843d6ec12ecc8
nandhu-44 <nandhu90040@gmail.com>
2024-04-07 10:25:23 +0530

    Vercel cors updated

--- ToDo/server/index.js
+++ ToDo/server/index.js
@@ -7,21 +7,6 @@ const routes = require('./routes/route');
 dotenv.config();
 
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
-    next();
-});
-
-app.use(cors(
-    // {
-    //     origin: ["*", "https://todo-nandhu.vercel.app/"],
-    //     methods: 'GET, POST, PUT, DELETE',
-    //     allowedHeaders: 'Content-Type, Authorization'
-    // }
-));
-
 
 console.clear();
 
